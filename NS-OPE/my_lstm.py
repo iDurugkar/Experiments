a023603d4ae9cc0c9fb9ddff799c2e59a71d73b4
idurugkar <idurugkar@cs.umass.edu>
2015-12-10 23:46:56 -0500

    Modifying to use a single neuron. Code Cleanup

--- NS-OPE/my_lstm.py
+++ NS-OPE/my_lstm.py
@@ -72,99 +72,58 @@ target_var = T.matrix(name='targets', dtype=theano.config.floatX)
 
 
 def build_net(inputVar):
-    # Batch size=None, Sequence_Length=None, number of inputs = num_inputs
     l_inp = InputLayer((None, sequence_size), input_var=inputVar)  # ((None, None, 1))
     # retrieve symbolic references to batch_size and sequence_length
     batch_size, sequence_length = l_inp.input_var.shape
     l_rshp1 = ReshapeLayer(l_inp, (batch_size, sequence_length, 1))
 
-    # retrieve symbolic references to batch_size and sequence_length
-    # batch_size, sequence_length, _ = l_rshp1.output_shape
-
-    # The LSTM layer!
-    # forget_gate = Gate(b=lasagne.init.Constant(0.3))
-    l_lstm = GRULayer(l_rshp1, num_units=num_units, learn_init=True)  # , forgetgate=forget_gate)
-
-    # l_lstm2 = LSTMLayer(l_lstm, num_units=num_units2, learn_init=True)  # , forgetgate=forget_gate)
-
-    # Basic RNN
-    # l_lstm = RecurrentLayer(l_rshp1, num_units=num_outputs, nonlinearity=nonlinearities.tanh) # num_units
-
-    # GRU layer
-    # l_lstm = GRULayer(l_rshp1, num_units=num_units)
+    l_recurr = GRULayer(l_rshp1, num_units=num_units, learn_init=True)
     # Flatten output of batch and sequence so that each time step
     # of each sequence is processed independently.
     # Didn't understand this part :/
-    l_rshp2 = ReshapeLayer(l_lstm, (-1, num_units))
+    l_rshp2 = ReshapeLayer(l_recurr, (-1, num_units))
     l_dense = DenseLayer(l_rshp2, num_units=num_outputs, nonlinearity=None)
     l_out = ReshapeLayer(l_dense, (batch_size, sequence_length))
 
-    return l_out, l_lstm
+    return l_out
 
 network, lstm = build_net(input_var)
 save_network, _ = build_net(input_var)
 
-# temp = train[1:2]
-
 prediction = lasagne.layers.get_output(network)
-# TODO: Replace input regularization with prev_prediction regularization
 loss = lasagne.objectives.squared_error(prediction, target_var)
-# reg = lasagne.objectives.squared_error(prediction, input_var)
-# reg = reg.mean() * 0.03
-# reg = regularize_network_params(network, l2) * 0.01
-loss = loss.mean()  # + reg
+loss = loss.mean()
 
 
 params = lasagne.layers.get_all_params(network, trainable=True)
 updates = lasagne.updates.adagrad(loss, params, learning_rate=0.1)
-# updates = lasagne.updates.adadelta(loss, params, learning_rate=0.1, rho=0.99)
 
 
-# generate_output = get_output(network, deterministic=True)
 test_prediction = get_output(network, deterministic=True)
 test_loss = lasagne.objectives.squared_error(test_prediction,
                                                         target_var)
 test_loss = test_loss.mean()
 
-# test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
-#                   dtype=theano.config.floatX)
 
 index = T.scalar(name='index', dtype='int64')
-# train_fn = theano.function(inputs=[index], outputs=loss,
-#                            givens={input_var: train[index],
-#                                    target_var: train_targets[index]},
-#                            updates=updates)
 train_fn = theano.function([input_var, target_var], loss, updates=updates, allow_input_downcast=True)
 val_fn = theano.function([input_var, target_var], test_loss, allow_input_downcast=True)
 
 num_epochs = 3000
 min_err = float('inf')
+min_epoch = 0
 
 for epoch in range(num_epochs):
-    # err = np.zeros((1, num_train), dtype=theano.config.floatX)
-    # for i in range(num_train):
     err = train_fn(full_train, full_targets)
     if err < min_err:
+        min_epoch = epoch
         min_err = err
         set_all_param_values(save_network, get_all_param_values(network))
     print('Epoch %d, Error %f' % (epoch, err))
 
-# pred_err = val_fn(test, test_targets)
-# print('Test error: %f' % pred_err)
-
-
+print('Creating Test Network on epoch %d for train error: %f' % (min_epoch, min_err))
 out = get_output(save_network)
 get_it = theano.function([input_var], out, allow_input_downcast=True)
-# with open('results.txt', 'w') as fw:
-#     for i in range(num_train):
-#         got_it = get_it(np.reshape(train[i], (1, train[i].shape[0])))
-#         fw.writelines([str((i+1)*sequence_size) + ' ' + str(got_it[0, -1] * normalizer) + '\n'])
-#     for i in range(num_test):
-#         got_it = get_it(np.reshape(test[i], (1, test[i].shape[0])))
-#         fw.writelines([str((num_train+i+1)*sequence_size) + ' ' + str(got_it[0, -1] * normalizer) + '\n'])
-#
-
-# dataset = dataset.reshape((1, -1))
 
 final_error = 0.
 abs_error = 0.
@@ -196,4 +155,3 @@ plt.clf()
 plt.plot(got_it[0]*normalizer, color='r')
 plt.plot(ind, evals, linestyle='-', color='b')
 plt.show()
-# plt.savefig('mars_10_500_nd.png')
